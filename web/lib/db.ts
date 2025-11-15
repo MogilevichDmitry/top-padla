d617e2e7d5e9d31d2c059bf3781c9f8c802bd63d
mogilevichdmitry <dmsmain@gmail.com>
2025-11-15 17:25:57 +0100

    Fix array handling in createMatch using string join

--- web/lib/db.ts
+++ web/lib/db.ts
@@ -57,8 +57,14 @@ export async function createMatch(match: Omit<Match, "id">): Promise<Match> {
     VALUES (
       ${match.date},
       ${match.type},
-      ARRAY[${sql.join(match.team_a.map(id => sql`${id}`), sql`, `)}]::integer[],
-      ARRAY[${sql.join(match.team_b.map(id => sql`${id}`), sql`, `)}]::integer[],
+      ARRAY[${sql.join(
+        match.team_a.map((id) => sql`${id}`),
+        sql`, `
+      )}]::integer[],
+      ARRAY[${sql.join(
+        match.team_b.map((id) => sql`${id}`),
+        sql`, `
+      )}]::integer[],
       ${match.score_a},
       ${match.score_b},
       ${match.created_by}
